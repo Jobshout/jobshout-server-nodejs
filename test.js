6d604b98eaef046a319b8ee38e575887ae660a50
neha-kapoor <nehakapoor@Nehas-MacBook-Air.local>
2016-07-15 18:08:58 +0530

    This is just for testing

---
+++ test.js
@@ -0,0 +1,15 @@
+	/**********************************************************************
+	*  Author: Neha Kapoor (neha@jobshout.org)
+	*  Project Lead: Balinder WALIA (bwalia@jobshout.org)
+	*  Project Lead Web...: https://twitter.com/balinderwalia
+	*  Name..: Jobshout Server NodeJS
+	*  Desc..: Jobshout Server (part of Jobshout Suite of Apps)
+	*  Web: http://jobshout.org
+	*  License: http://jobshout.org/LICENSE.txt
+	**/
+
+	/**********************************************************************
+	*  test.js handles the http requests
+	**/
+
+console.log("test file")
